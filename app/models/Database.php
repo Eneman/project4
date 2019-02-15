9073f9836b5fdb2af6a9c2f41eaf49f3055df270
Eneman <hendrix.bcz@gmail.com>
2019-02-15 15:12:59 +0100

    [models] Reworked Database model

--- app/models/Database.php
+++ app/models/Database.php
@@ -1,15 +1,24 @@
-// Gere les connexions a la bdd + operations CRUD
-
 <?php
     abstract class Database
     {
         private $db;
+
+        private function connectDB()
+        {
+            if($this->db == null)
+            {
+                $this->db = new PDO('mysql:host=localhost;dbname=blog_jf;charset=utf8', 'root', '',
+                array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            }
+            return $this->db;
+        }
 
         protected function exeRequest($sql, $args = null)
         {
             if ($args == null)
             {
                 $result = $this->connectDB()->query($sql);
+                
             }
             else
             {
@@ -20,13 +29,4 @@
             return $result;
         }
 
-        private function connectDB()
-        {
-            if($this->db == null)
-            {
-                this->db = new PDO('mysql:host=localhost;dbname=blog_jf;charset=utf8', 'root', '',
-                array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-            }
-            return $this->db;
-        }
     }
