96cd3ab83659331f3c14942640e03f18c924d7cb
Eneman <hendrix.bcz@gmail.com>
2019-02-22 15:26:25 +0100

    [template] Added header.php and footer.php as template

---
+++ app/views/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Maquette Graphique</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo BASE_URL; ?>/temp/main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <div class="header">
+        <h1>jeAn forteroche</h1>
+        <nav class="navigation_menu">
+            <div><a href="http://home">Accueil</a></div>
+            <div><a href="http://chapters">Chapitres</a></div>
+            <div><a href="<?php echo BASE_URL; ?>/contact/view">Contacts</a></div>
+            <div><a href="http://about">A propos</a></div>
+        </nav>
+    </div>  
