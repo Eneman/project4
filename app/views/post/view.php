b33d39b2b5f100abe6c890d45def5c68a1722f35
Eneman <hendrix.bcz@gmail.com>
2019-03-22 10:22:29 +0100

    [comment] Added comments and form to view

--- app/views/post/view.php
+++ app/views/post/view.php
@@ -6,4 +6,22 @@
     <p class="post_content"><?php echo $post['post_content']; ?></p>
 
 </section>
+
+<section class="comments_container">
+    <form method="post" action="<?php echo BASE_URL; ?>/post/comment/<?php echo $post["post_id"];?> class="comments">
+    Nom : <input type="text" name="com_author" required>
+    <textarea name="com_content" cols="100" rows="10" placeholder="Taper votre commentaire" required ></textarea>
+    
+    <input type="submit" value="Envoyer">
+    </form>
+
+    <?php foreach($comments as $comment) {?>
+        <article class="post_container" id="<?php echo $post['com_id']?>">
+            <h3 class="com_author"><?php echo $comment['com_author']; ?></h3>
+            <p class="com_report"><i class="fas fa-exclamation-triangle"></i><?php echo $comment['com_report']; ?></p>
+            <p class="com_content"><?php echo $comment['com_content']; ?> ...</p>
+            <p class="com_date"><?php echo $comment['com_date']; ?></p>
+        </article>
+    <?php } ?>
+</section>
 <?php require_once 'app/views/footer.php'; ?>
