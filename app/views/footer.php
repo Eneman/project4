dc6355b5f407c37d7ca188fef6561f93e93634a1
Eneman <hendrix.bcz@gmail.com>
2019-03-29 12:58:47 +0100

    [front] Clickable Title, sessions and IP tests

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,5 +1,7 @@
         <footer class="footer">
-
+            <?php var_dump($_SERVER['REMOTE_ADDR']) ?>
         </footer>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="/FORMATION/PROJET4/public/assets/report.js"></script>
     </body>
 </html>
--- app/views/header.php
+++ app/views/header.php
@@ -1,6 +1,3 @@
-<?php session_start();
-    echo $_SESSION['test'];
-    ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,13 +7,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="<?php echo BASE_URL; ?>/public/assets/main.css" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <script src="https://cloud.tinymce.com/5/tinymce.min.js?apiKey=9qrbi8545615l7v6s565owh9c30xo5ubpfssxlch2pvj07mo">
+    </script>
+    <script>
+        tinymce.init({
+            selector: '.editor'
+        });
+    </script>
 </head>
 <body>
     <div class="header">
-        <h1>jeAn forteroche</h1>
+        <h1><a href="<?php echo BASE_URL; ?>/post/list">jeAn forteroche</a></h1>
         <nav class="navigation_menu">
             <div><a href="<?php echo BASE_URL; ?>/post/list">Accueil</a></div>
             <div><a href="<?php echo BASE_URL; ?>/contact/view">Contacts</a></div>
-            <div><a href="<?php echo BASE_URL; ?>/contact/view">A propos</a></div>
+            <div><a href="<?php echo BASE_URL; ?>/about/view">A propos</a></div>
+            <div><a href="<?php echo BASE_URL; ?>/admin/index">Administration</a></div>
         </nav>
     </div>  
